43ee82ba18a20f8a1ad92425aa5fc1b7beff5a6c
larissa <larissa.queiroz95@gmail.com>
2022-05-10 00:51:00 -0300

    criação e registro dos models

--- app/admin.py
+++ app/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
-
+from .models import Arma, Municao, Objeto, Objeto_Tipo, Calibre
 # Register your models here.
+
+class ArmaAdmin(admin.ModelAdmin):
+    list_display = ('id', 'calibre_id', 'marca', 'modelo', 
+                                'quantidade_de_tiros', 'valor_estimado', 'imagem')
+    list_filter = ('marca', 'modelo', 'quantidade_de_tiros', 'valor_estimado')
+
+class MunicaoAdmin(admin.ModelAdmin):
+    list_display = ('id', 'calibre_id', 'marca', 'modelo', 
+                                'valor_estimado')
+    list_filter = ('marca', 'modelo', 'valor_estimado')
+
+admin.site.register(Arma, ArmaAdmin)
+admin.site.register(Municao, MunicaoAdmin)
+admin.site.register(Objeto)
+admin.site.register(Objeto_Tipo)
+admin.site.register(Calibre)
--- app/models.py
+++ app/models.py
@@ -1,3 +1,41 @@
 from django.db import models
+from django.forms import CharField, IntegerField
 
 # Create your models here.
+class Calibre(models.Model):
+    desc_calibre = models.CharField(max_length=45)
+
+    def __str__(self):
+        return self.desc_calibre
+
+class Objeto_Tipo(models.Model): # TODO: verificar o tipo do id!!!!!!
+    tipo_de_objeto = models.CharField(max_length=64)
+
+    def __str__(self):
+        return self.tipo_de_objeto
+
+class Arma(models.Model):
+    calibre_id = models.ForeignKey(Calibre, on_delete=models.DO_NOTHING)
+    marca = models.CharField(max_length=64)
+    modelo = models.CharField(max_length=64)
+    quantidade_de_tiros = models.IntegerField()
+    valor_estimado = models.DecimalField(decimal_places=2,max_digits=10)
+    imagem = models.CharField(max_length=128)
+
+    def __str__(self):
+        return self.modelo
+
+class Municao(models.Model):
+    calibre_id = models.ForeignKey(Calibre, on_delete=models.DO_NOTHING)
+    marca = models.CharField(max_length=64)
+    modelo = models.CharField(max_length=64)
+    valor_estimado = models.DecimalField(decimal_places=2,max_digits=10)
+
+    def __str__(self):
+        return self.modelo
+
+class Objeto(models.Model):
+    objeto_tipo_id = models.SmallIntegerField()
+
+    def __str__(self):
+        return self.objeto_tipo_id
